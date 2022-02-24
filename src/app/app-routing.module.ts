0ddc0d89d94e044b356d21fea3b3ccede2eb6321
olu <olugboyegaonimole@gmail.com>
2022-02-24 04:38:51 +0000

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
 
-const routes: Routes = [];
+import { DefaultComponent } from './admin/default/default.component';
+import { SidebarComponent } from './admin/shared/sidebar/sidebar.component';
+
+const routes: Routes = [
+  {path:'', component:DefaultComponent,
+  children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'posts', component: PostsComponent}
+  ]
+
+},
+  {path:'test', component:SidebarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
